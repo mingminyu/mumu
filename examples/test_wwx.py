b703ac096ca33eb09c2f9c621f3cc4d1e66efa20
mingminyu <yu_mingm623@163.com>
2021-07-05 23:30:12 +0800

    update robots/_workwx.py

--- examples/test_wwx.py
+++ examples/test_wwx.py
@@ -1,33 +1,33 @@
-# coding: utf-8
-# ================================================
-# Project: mumu
-# File: /test_wwx.py
-# Author: Mingmin Yu
-# Email: yu_ming623@163.com
-# Date: 2021/6/23 19:35
-# Description:
-# ================================================
+# # coding: utf-8
+# # ================================================
+# # Project: mumu
+# # File: robots/test_wwx.py
+# # Author: Mingmin Yu
+# # Email: yu_ming623@163.com
+# # Date: 2021/6/23 19:35
+# # Description:
+# # ================================================
 import os
+import textwrap
 from mumu.conf import *
-from mumu.robots import WorkWeChatRobot
+from mumu.robots import *
 
-# Test 1: Test for end text messages and at_all -> success
+# # Test 1: Test for end text messages and at_all -> success
 # content = """
 #     Hi，我是机器人test
 #     由郁明敏于2020年12月18日添加到群
 #     """
-# wwx = WorkWeChatRobot(key=WWX_CONF["key"], at_all=True)
-# wwx.send_messages(content=content)
-
-# Test 2: Test for mentioned_mobile_list -> success
+# wwx_send_messages(key=WWX_CONF["key"], at_all=True, content=content)
+#
+# # Test 2: Test for mentioned_mobile_list -> success
 # content = """
 #     Hi，我是机器人test2
 #     由郁明敏于2020年12月18日添加到群
 #     """
-# wwx = WorkWeChatRobot(key=WWX_CONF["key"], mentioned_mobile_list=WWX_CONF["at"])
-# wwx.send_messages(content=content)
-
-# Test 3: Test for markdown messages -> success
+# wwx_send_messages(key=WWX_CONF["key"], content=content, mentioned_mobile_list=WWX_CONF["at"])
+#
+#
+# # Test 3: Test for markdown messages -> success
 # content = '''实时新增用户反馈<font color=\"warning\">132例</font>，请相关同事注意。\n
 #          > 类型:<font color=\"comment\">用户反馈</font>
 #          > 普通用户反馈:<font color=\"comment\">117例</font>
@@ -44,16 +44,14 @@ from mumu.robots import WorkWeChatRobot
 #
 #          [click to browser](http://work.weixin.qq.com/api/doc)
 #         '''
-# wwx = WorkWeChatRobot(key=WWX_CONF["key"], at_all=True, mentioned_mobile_list=WWX_CONF["at"])
-# wwx.send_messages_md(content=content)
+# wwx_send_messages_md(key=WWX_CONF["key"], content=textwrap.dedent(content))
 
 # Test 4: Test for image -> success
-# img = os.path.join(os.getcwd(), "zoro.jpg")
-# wwx = WorkWeChatRobot(key=WWX_CONF["key"])
-# wwx.send_image(image_path=img)
+img = os.path.join(os.getcwd(), "zoro.jpg")
+wwx_send_image(key=WWX_CONF["key"], image=img)
 
 # Test 4: Test for news -> success
-# content = [
+# articles = [
 #         {
 #            "title": "中秋节礼品领取",
 #            "description": "今年中秋节公司有豪礼相送",
@@ -67,10 +65,8 @@ from mumu.robots import WorkWeChatRobot
 #            "picurl": "https://img2.baidu.com/it/u=3566088443,3713209594&fm=26&fmt=auto&gp=0.jpg"
 #         },
 #     ]
-# wwx = WorkWeChatRobot(key=WWX_CONF["key"])
-# wwx.send_article(content=content)
+# wwx_send_news(key=WWX_CONF["key"], articles=articles)
 
-# Test 5: Test for file ->
-img = os.path.join(os.getcwd(), "zoro.jpg")
-wwx = WorkWeChatRobot(key=WWX_CONF["key"])
-wwx.send_file(file=img)
+# Test 5: Test for file -> success
+# img = os.path.join(os.getcwd(), "zoro.jpg")
+# wwx_send_file(key=WWX_CONF["key"], file=img)
--- mumu/robots/_workwx.py
+++ mumu/robots/_workwx.py
@@ -15,145 +15,11 @@ import hashlib
 from mumu.utils import str_to_list
 
 
-class WorkWeChatRobot(object):
-    def __init__(self, key=None, mentioned_mobile_list=None, at_all=False):
-        """
-
-        :param key:
-        :param msgtype: text / markdown
-        :param mentioned_list: list
-        :param mentioned_mobile_list: list
-        """
-        prefix_api = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key="
-        upload_api = "https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?key=%s&type=file"
-        self.web_hook = prefix_api + key
-        self.upload_hook = upload_api % key
-        self.mentioned_mobile_list = mentioned_mobile_list
-        self.at_all = at_all
-        self.headers = {
-            "Content-Type": "multipart/form-data;text/plain;application/json;charset=UTF-8",
-            "Accept": "application/json;charset=UTF-8",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/79.0.3945.79 Safari/537.36 "
-            }
-
-    def _preload(self, msgtype=None, content=None, base64_=None, md5_=None, media_id=None):
-        """preload send messages context"""
-        preload = {"msgtype": msgtype}
-
-        if msgtype == "text":
-            preload[msgtype] = {
-                "mentioned_mobile_list": str_to_list(self.mentioned_mobile_list) if not self.at_all else ["@all"],
-                "content": content
-                }
-        elif msgtype == "markdown":
-            preload[msgtype] = {
-                "content": content
-                }
-        elif msgtype == "image":
-            preload[msgtype] = {
-                "base64": base64_,
-                "md5": md5_
-                }
-        elif msgtype == "news":
-            preload[msgtype] = {
-                "articles": content
-                }
-        elif msgtype == "file":
-            preload[msgtype] = {
-                "media_id": media_id
-                }
-        else:
-            preload = {}
-
-        return preload
-
-    def send_messages(self, content=None):
-        preload = self._preload(msgtype="text", content=content)
-        res = requests.post(self.web_hook, json=preload, headers=self.headers, verify=False)
-
-        if json.loads(res.text)["errmsg"] == "ok":
-            logging.info("successful")
-        else:
-            logging.info("failed")
-
-    def send_messages_md(self, content=None):
-        """
-
-        :param content:
-        :return:
-        """
-        preload = self._preload(msgtype="markdown", content=content)
-        res = requests.post(self.web_hook, json=preload, headers=self.headers, verify=False)
-
-        if json.loads(res.text)["errmsg"] == "ok":
-            logging.info("successful")
-        else:
-            logging.info("failed")
-
-    def send_image(self, image_path=None):
-        """Send an image
-
-        :param image_path: str, absolute path of an image
-        :return:
-        """
-        with open(image_path, "rb") as img:
-            base64_ = base64.b64encode(img.read())
-
-        with open(image_path, "rb") as img:
-            md = hashlib.md5()
-            md.update(img.read())
-            md5_ = md.hexdigest()
-
-        preload = self._preload(msgtype="image", base64_=base64_, md5_=md5_)
-        res = requests.post(self.web_hook, json=preload, headers=self.headers, verify=False)
-
-        if json.loads(res.text)["errmsg"] == "ok":
-            logging.info("successful")
-        else:
-            logging.info("failed")
-
-    def send_images(self, images_path=None):
-        """
-
-        :param images_path: list, absolute path of images
-        :return:
-        """
-        for img in images_path:
-            self.send_image(img)
-
-    def send_article(self, content=None):
-        preload = self._preload(msgtype="news", content=content)
-        res = requests.post(self.web_hook, json=preload, headers=self.headers, verify=False)
-
-        if json.loads(res.text)["errmsg"] == "ok":
-            logging.info("successful")
-        else:
-            logging.info("failed")
-
-    def send_file(self, file):
-        media_id = self._upload_file(file)
-        preload = self._preload(msgtype="file", media_id=media_id)
-        res = requests.post(self.web_hook, json=preload)
-
-        if json.loads(res.text)["errmsg"] == "ok":
-            logging.info("successful")
-        else:
-            logging.info("failed")
-
-    def _upload_file(self, file):
-        with open(file, "rb") as f:
-            data = {"file": f}
-            res = requests.post(self.upload_hook, files=data)
-            media_id = res.json()["media_id"]
-
-            return media_id
-
-
 prefix_api = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=%s"
 upload_api = "https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?key=%s&type=file"
+# multipart/form-data;
 headers = {
-    "Content-Type": "multipart/form-data;text/plain;application/json;charset=UTF-8",
+    "Content-Type": "application/json;text/plain;charset=UTF-8",
     "Accept": "application/json;charset=UTF-8",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/79.0.3945.79 Safari/537.36 "
@@ -161,6 +27,15 @@ headers = {
 
 
 def send_messages(key=None, content=None, at_all=False, mentioned_mobile_list=None):
+    """Send messages in text format.
+
+    :param key:str, private key for Work WeChat robot
+    :param content: str, text messages will be sent.
+    :param at_all: bool, default False, robot will @all when set `True`
+    :param mentioned_mobile_list: use mobile list to remind some members.
+    :return: bool
+        True for success, False for fail.
+    """
     web_hook = prefix_api % key
     preload = {
         "msgtype": "text",
@@ -169,20 +44,24 @@ def send_messages(key=None, content=None, at_all=False, mentioned_mobile_list=No
             "content": content
             }
         }
+    print(preload)
     res = requests.post(web_hook, json=preload, headers=headers, verify=False)
 
     if json.loads(res.text)["errmsg"] == "ok":
         logging.info("successful")
+        return True
     else:
         logging.info("failed")
+        return False
 
 
 def send_messages_md(key=None, content=None):
-    """
+    """Send messages in markdown format.
 
-    :param key:
-    :param content:
-    :return:
+    :param key: str, private key for Work WeChat robot
+    :param content: str, markdown messages will be sent.
+    :return: bool
+        True for success, False for fail.
     """
     web_hook = prefix_api % key
     preload = {
@@ -196,6 +75,7 @@ def send_messages_md(key=None, content=None):
 
     if json.loads(res.text)["errmsg"] == "ok":
         logging.info("successful")
+        return True
     else:
         logging.info("failed")
 
@@ -203,12 +83,13 @@ def send_messages_md(key=None, content=None):
 def send_image(key=None, image=None):
     """Send an image
 
-    :param key: str,
+    :param key: str, key for Work WeChat robot
     :param image: str, absolute path of an image
-    :return:
+    :return: bool
+        True for success, False for fail.
     """
     with open(image, "rb") as img:
-        base64_ = base64.b64encode(img.read())
+        base64_ = str(base64.b64encode(img.read()), encoding='utf-8')
 
     with open(image, "rb") as img:
         md = hashlib.md5()
@@ -220,16 +101,20 @@ def send_image(key=None, image=None):
         "msgtype": "image",
         "image": {
             "base64": base64_,
-            "md5": md5_
+            "md5": md5_,
             }
         }
+
+    headers = {"Content-Type": "application/json"}
 
     res = requests.post(web_hook, json=preload, headers=headers, verify=False)
 
     if json.loads(res.text)["errmsg"] == "ok":
         logging.info("successful")
+        return True
     else:
         logging.info("failed")
+        return False
 
 
 def send_images(key=None, images=None):
@@ -237,18 +122,26 @@ def send_images(key=None, images=None):
 
     :param key: str
     :param images: list, absolute path of images
-    :return:
+    :return: bool
+        True for success, False for fail.
     """
     for img in images:
         send_image(key=key, image=img)
 
 
 def send_news(key=None, articles=None):
+    """Send news with links
+
+    :param key: str, key for Work WeChat robot
+    :param articles: list, a list contains json articles
+    :return: bool
+        True for success, False for fail.
+    """
     web_hook = prefix_api % key
     preload = {
         "msgtype": "news",
         "news": {
-            "artcles": articles
+            "articles": articles
         }
     }
 
@@ -256,16 +149,25 @@ def send_news(key=None, articles=None):
 
     if json.loads(res.text)["errmsg"] == "ok":
         logging.info("successful")
+        return True
     else:
         logging.info("failed")
+        return False
 
 
 def send_file(key=None, file=None):
+    """Send a file
+
+    :param key: str, key for Work WeChat robot
+    :param file: str, file absolute path
+    :return: bool
+        True for success, False for fail.
+    """
     web_hook = prefix_api % key
     media_id = _upload_file(key=key, file=file)
     preload = {
         "msgtype": "file",
-        "news": {
+        "file": {
             "media_id": media_id
             }
         }
@@ -273,11 +175,20 @@ def send_file(key=None, file=None):
 
     if json.loads(res.text)["errmsg"] == "ok":
         logging.info("successful")
+        return True
     else:
         logging.info("failed")
+        return False
 
 
 def _upload_file(key=None, file=None):
+    """Upload
+
+    :param key: str, key for Work WeChat robot
+    :param file: str, file absolute path
+    :return: str,
+        return media id of the file when upload successfully.
+    """
     upload_hook = upload_api % key
 
     with open(file, "rb") as fp:
